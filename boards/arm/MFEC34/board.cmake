432266e0f43e2533f63c5ff22fcd02e6ea72b0a6
chandler5314 <tashi.chandler@gmail.com>
2025-02-28 03:16:45 +0000

    change board cmake use nfr j prog  delete Jlink

--- boards/arm/MFEC34/board.cmake
+++ boards/arm/MFEC34/board.cmake
@@ -3,4 +3,5 @@
 #board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
