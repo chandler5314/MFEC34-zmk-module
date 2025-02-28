6d90224de435c14c30487f25d39b6bc3e026ef39
chandler5314 <tashi.chandler@gmail.com>
2025-02-28 03:04:17 +0000

    board make board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")

--- boards/arm/MFEC34/board.cmake
+++ boards/arm/MFEC34/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
 
-board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
